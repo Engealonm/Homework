842d7347f7d15653c0e5d12904553939377d61d6
Engea <]zekenov05@list.ru>
2024-02-29 12:27:22 +0600

    second commit

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,14 @@
 public class Main {
     public static void main(String[] args) {
         Youtube_channels new_channel = new Youtube_channels();
-        new_channel.addchannel("First Java position");
-        new_channel.addchannel("Second Java position");
+        new_channel.addchannel("nurzatguides");
+        new_channel.addchannel("Rusgametactics");
         Observer Firstsubcriber = new Subcriber("Miko");
         Observer Secondsubcriber = new Subcriber("Aiko");
         new_channel.register(Firstsubcriber);
         new_channel.register(Secondsubcriber);
-        new_channel.addchannel("Third Java Position");
-        new_channel.removechannel("First Java position");
+        new_channel.addchannel("Padlo play");
+
 
     }
 }
